8959589c2fa164eedf8442554bad673e48c66494
charles <charles_spfc@hotmail.com>
2023-08-26 07:45:14 -0300

    Add colors tailwind

--- fullstack_api_starwars_front_end/tailwind.config.js
+++ fullstack_api_starwars_front_end/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        defaultRed: "#f9004d",
+        hoverdefaultRed: "#ffcdb8",
+        darkTheme: "#282828",
+        backGroundColorDarkTheme: "#404040",
+      },
+    },
   },
   plugins: [],
 };
